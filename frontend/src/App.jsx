4759b198c7279eb47203cd10ef473a17ebb72f96
RicardoBachmann <ricardo.bachmann@icloud.com>
2025-02-21 08:30:51 +0100

    Set fetch request in frontend for satellite data... backend still not working correctly

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,6 +19,32 @@ function App() {
     };
   }, []);
 
+  const API_URL =
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : "https://maschinensehen.vercel.app/";
+
+  const fetchSatelliteData = async () => {
+    try {
+      const lon = 10;
+      const lat = 50;
+      const alt = 100;
+      const num = 1;
+      const id = 25544;
+
+      const response = await fetch(
+        `${API_URL}/api/satellite/position/${lon}/${lat}/${alt}/${num}/${id}`
+      );
+      if (!response.ok) {
+        throw new Error("Network response failed");
+      }
+      const data = await response.json();
+      console.log("Satellite-Data:", data);
+    } catch (error) {
+      console.error("Error retrieving satellite data:", error);
+    }
+  };
+
   return (
     <>
       <div id="map-container" ref={mapContainerRef}></div>
